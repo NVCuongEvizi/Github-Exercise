60cc358093f9d8eaeb20073af2356aba58dc615c
NVCuongEvizi <cuong.v.nguyen@evizi.com>
2022-10-04 14:54:47 +0700

    fix Home2

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <a>Home</a>
+            <p>Home 222222</p>
+        </div>
     )
 }
 
