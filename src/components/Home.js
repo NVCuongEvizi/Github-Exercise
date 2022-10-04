aa19ad44438ab70b798752a78fbd40389e72998f
NVCuongEvizi <cuong.v.nguyen@evizi.com>
2022-10-04 14:55:55 +0700

    push home2 cherry

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <a>Home</a>
+            <p>Home 222222</p>
+            <p>Home 333333</p>
+        </div>
+    )
+}
+
+export default Home
